a41a5d0f7fa352a180c263cc6fabed2ebd95e94e
imura <imura2005@gmail.com>
2017-10-18 15:19:28 +0900

    change

--- app/src/main/java/com/example/imura/pictureedit/MainActivity.kt
+++ app/src/main/java/com/example/imura/pictureedit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.imura.pictureedit
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
@@ -35,11 +36,24 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private void launchCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+    abstract var mCurrentPhotoPath: String
 
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        val fileUri = contentResolver
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null) {
+            mCurrentPhotoPath = fileUri.toString()
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        }
     }
+
 
 
 
